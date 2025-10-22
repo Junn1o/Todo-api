641e353653ed4a1a501f2ea4d15b1e1287505589
Junnio <nphan218@gmail.com>
2025-10-22 22:51:19 +0700

    Update change

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using DotNetEnv;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+DotNetEnv.Env.Load();
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
