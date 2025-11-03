ee2cc3b86f1d38c4bc79b3aa711b30c2fbcffe6c
Junnio <nphan218@gmail.com>
2025-11-03 19:51:37 +0700

    refractor code

--- Models/DTO/CategoryDTO.cs
+++ Models/DTO/CategoryDTO.cs
@@ -1,17 +1,17 @@
 ﻿using Todo_api.Models.Domain;
 namespace Todo_api.Models.DTO
 {
-    public class CategoryWithTaskDTO
+    public class UserCategoryWithTaskDTO
     {
-        public Guid CategoryId { get; set; }
-        public required string CategoryName { get; set; }
-        public Guid UserId { get; set; }
-        public List<TasksDTO> Tasks { get; set; }
-        public List<CategoryWithTaskDTO> categoryWithTaskDTO { get; set; }
+        public Guid category_id { get; set; }
+        public Guid user_id { get; set; }
+        public required string category_name { get; set; }
+        public List<TasksDTO> task_list { get; set; }
         public class TasksDTO
         {
             public Guid task_id { get; set; }
             public string title { get; set; }
+            public string user_name { get; set; }
             public string? note { get; set; }
             public string? is_completed { get; set; }
             public string? is_repeat { get; set; }
@@ -20,28 +20,15 @@ namespace Todo_api.Models.DTO
             public string? is_notification { get; set; }
             public string due_date { get; set; }
             public string modify_date { get; set; }
-            public string category_name { get; set; }
         }
 
         public int totalResult { get; set; }
         public int totalPages { get; set; }
     }
-    public class CategoryRequestFromDTO 
+    public class CategoryRequestFromDTO
     {
-        public required string CategoryName { get; set; }
-    }
-    public class CategoryCreateDTO
-    {
-        public string CategoryName { get; set; }
-        public Guid UserId { get; set; }
-    }
-    public class CategoryUpdateDTO
-    {
-        public Guid CategoryId { get; set; }
-        public string CategoryName { get; set; }
-    }
-    public class CategoryDeleteDTO
-    {
-        public Guid CategoryId { get; set; }
+        public required string category_name { get; set; }
+        public required Guid user_id { get; set; }
+        public Guid category_id { get; set; }
     }
 }
--- Models/DTO/TaskDTO.cs
+++ Models/DTO/TaskDTO.cs
@@ -2,32 +2,44 @@
 
 namespace Todo_api.Models.DTO
 {
-    public class TaskWithAllSub_Task
+    public class GetAllTask
     {
-        public Guid TaskID { get; set; }
-        public required string Title { get; set; }
-        public string? Note { get; set; }
-        public bool? IsCompleted { get; set; }
-        public bool? IsRepeat { get; set; }
-        public bool? IsImportance { get; set; }
-        public string? Progress { get; set; }
-        public string? IsNotification { get; set; }
-        public required DateTime DueDate { get; set; }
-        public DateTime? ModifyDate { get; set; }
-        public Guid? UserId { get; set; }
-        public Guid? CategoryID { get; set; }
-        public List<Sub_Task> Sub_Tasks { get; set; }
-        public List<TaskWithAllSub_Task> taskWithAllSub_Task { get; set; }
+        public Guid task_id { get; set; }
+        public string title { get; set; }
+        public string user_name { get; set; }
+        public string note { get; set; }
+        public string is_completed { get; set; }
+        public string is_repeat { get; set; }
+        public string is_importance { get; set; }
+        public string progress { get; set; }
+        public string is_notification { get; set; }
+        public string due_date { get; set; }
+        public string modify_date { get; set; }
+    }
+    public class GetTaskById
+    {
+        public Guid task_id { get; set; }
+        public string title { get; set; }
+        public string note { get; set; }
+        public bool is_completed { get; set; }
+        public bool is_repeat { get; set; }
+        public bool is_importance { get; set; }
+        public string progress { get; set; }
+        public string is_notification { get; set; }
+        public required DateTime due_date { get; set; }
+        public DateTime modify_date { get; set; }
+        public Guid user_id { get; set; }
+        public string user_name { get; set; }
+        public Guid category_id { get; set; }
+        public string category_name { get; set; }
+        public List<Sub_Task> subtask_list { get; set; }
         public class Sub_Task
         {
-            public Guid SubTaskId { get; set; }
-            public Guid TaskId { get; set; }
-            public required string Name { get; set; }
-            public bool? IsCompleted { get; set; }
-            public required int Position { get; set; }
+            public Guid subtask_id { get; set; }
+            public string name { get; set; }
+            public bool is_completed { get; set; }
+            public int position { get; set; }
         }
-        public int totalResult { get; set; }
-        public int totalPages { get; set; }
     }
     public class TaskCreateDTO
     {
